46eaf5e5fe3f02b73e91134a1d175e728ff6cfb4
paul3042 <173724663+paul3042@users.noreply.github.com>
2025-06-02 19:53:09 +0100

    finalise

--- backend/db/config.js
+++ backend/db/config.js
@@ -6,7 +6,7 @@ const { Pool } = pg;
 const pool = new Pool(
     {
         connectionString: process.env.DB_URL,
-        ssl: isProd() ? { rejectUnauthorized: false } : false
+        ssl: isProd() ? { rejectUnauthorized: true } : false
 
     }
 );
